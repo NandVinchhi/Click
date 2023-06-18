968b35f4df774ce36e31f73160be05edf8f0bcb4
Nand Vinchhi <nand@getairchat.com>
2023-06-18 05:33:29 +0530

    endpoint change

--- frontend/pages/dashboard.jsx
+++ frontend/pages/dashboard.jsx
@@ -123,7 +123,7 @@ export default function App() {
         const formData = new FormData();
         formData.append("description", createDescription);
         fetch(
-          "http://127.0.0.1:8000/create",
+          "https://nandvinchhi.serveo.net/create",
           {
             method: "POST",
             body: formData,
